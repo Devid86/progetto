4b6cd19394b05b8f4a632e00341acb892a01f741
Devid Splendiani <s1087057@studenti.univpm.it>
2020-09-17 16:53:19 +0200

    correzione warnings

--- progetto/src/main/java/it/univpm/progetto/controller/ControllerClass.java
+++ progetto/src/main/java/it/univpm/progetto/controller/ControllerClass.java
@@ -8,11 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.deser.impl.CreatorCandidate.Param;
-
 import it.univpm.progetto.model.DropboxFile;
-import it.univpm.progetto.model.Statistics;
 import it.univpm.progetto.service.ServiceResponse;
 
 /**
